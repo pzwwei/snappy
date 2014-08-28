79841c247032d6f77c56ba1f6ae30219caf4e9c5
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2014-08-28 20:22:58 +0200

    Merge ConstraintProducer and AttributeType

--- Source/SNPAttributeType.swift
+++ Source/SNPAttributeType.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 /// The SNPAttributeType protocol provides a common interface for types that
-/// represent a layout attribute of a view.
+/// represent a layout attribute of a view and are capable of producing
+/// layout constraints.
 public protocol SNPAttributeType {
+    
+    /// The supported type of a constant factor in the expression.
+    typealias ConstantType
     
     /// A view associated with an attribute.
     var view: UIView { get }
@@ -18,5 +22,13 @@ public protocol SNPAttributeType {
     /// A foundation layout attribute. This property can be nil only when using
     /// with custom attributes, otherwise a fatal error may occur.
     var attribute: NSLayoutAttribute? { get }
+    
+    /// Produces the layout constraints using the given expression.
+    ///
+    /// :param: relation The relation between two sides of an equation.
+    /// :param: expression The expression to be used to construct a constraint.
+    ///
+    /// :returns: An array of installable layout constraints.
+    func produceConstraints(#relation: NSLayoutRelation, expression: SNPExpression<ConstantType>) -> [SNPConstraint]
     
 }
