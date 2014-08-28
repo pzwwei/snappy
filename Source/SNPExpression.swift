aa9a2eb9a8d631198d73ad4515ccf31f0d3491b2
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2014-08-29 01:14:19 +0200

    Merge expression files

--- Source/SNPExpression.swift
+++ Source/SNPExpression.swift
@@ -40,3 +40,28 @@ public struct SNPExpression <A: SNPAttributeType> {
     }
     
 }
+
+// /////////////////////////////////////////////////////////////////////////////
+
+/// The SNPAnonymousExpression structure is used to describe literal values as
+/// the right-hand side of a constraint equation.
+///
+/// :param: C The type of a constant value held by the expression.
+public struct SNPAnonymousExpression <C> {
+    
+    /// The constant factor in the expression.
+    public let constantValue: C
+    
+    /// The priority of the constraint.
+    public var priority: UILayoutPriority = 1000
+    
+    // /////////////////////////////////////////////////////////////////////////
+    
+    /// Creates and initializes an anonymous expression.
+    ///
+    /// :param: The constant factor in the expression.
+    init(constantValue: C) {
+        self.constantValue = constantValue
+    }
+    
+}
