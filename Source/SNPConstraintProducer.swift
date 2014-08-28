4261bc132947e7f71d4e9ca38d3a240c1f0d1ed3
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2014-08-28 19:59:37 +0200

    Add ConstantType to ConstraintProducer

--- Source/SNPConstraintProducer.swift
+++ Source/SNPConstraintProducer.swift
@@ -12,14 +12,15 @@ import UIKit
 /// capable of producing constraints.
 public protocol SNPConstraintProducer {
     
+    /// The supported type of a constant factor in the expression.
+    typealias ConstantType
+    
     /// Produces the layout constraints using the given expression.
     ///
     /// :param: relation The relation between two sides of an equation.
     /// :param: expression The expression to be used to construct a constraint.
     ///
     /// :returns: An array of installable layout constraints.
-    func produceConstraints(#relation: NSLayoutRelation, expression: String) -> [SNPConstraint]
-    
-    // FIXME: produceConstraints(relation:expression:) - String -> Expression
+    func produceConstraints(#relation: NSLayoutRelation, expression: SNPExpression<ConstantType>) -> [SNPConstraint]
     
 }
